e0661fd43c071db3860021f6468d7f2f82014f00
Trung Tran <s3891724@rmit.edu.vn>
2022-06-20 15:50:26 +0700

    test db connection

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     $connection = mysqli_connect("us-cdbr-east-05.cleardb.net", "a704e0507b7306", "41c3cb98", "heroku_f8f2d91b657802f");
 
     if (mysqli_connect_errno()) {
-        echo "failed to connect db " . mysqli_connect_error();
+        echo ("failed to connect db " . mysqli_connect_error());
     } else{
         echo "successfully connected";
     }
