bb20ccffe0bf739a74661eaefaa27975f81a3569
Trung Tran <s3891724@rmit.edu.vn>
2022-06-20 15:43:12 +0700

    test db connection

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
     echo "hola";
+    $connection = mysqli_connect("us-cdbr-east-05.cleardb.net", "b704e0507b7306", "41c3cb98", "heroku_f8f2d91b657802f");
+
+    if (mysqli_connect_errno()) {
+        echo "failed to connect db " . mysqli_connect_error();
+    }
 ?>
