f01adf9c3ad0904be5ab9d0df0a9bb30c1c0b7a9
Trung Tran <s3891724@rmit.edu.vn>
2022-06-25 17:47:09 +0700

    create structure folders

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
+
 <?php
     // $connection = mysqli_connect("us-cdbr-east-05.cleardb.net", "b704e0507b7306", "41c3cb98", "heroku_f8f2d91b657802f");
     $connection = mysqli_connect("localhost", "root", "Tranquoctrung224!", "test");
-
-    
 
     if (mysqli_connect_error()) {
         die("Database connection failed: " . mysqli_connect_error());
@@ -10,21 +9,29 @@
         echo "successfully connected";
     }
 
-    $sql = "SELECT * FROM employee";
-    $result = $connection -> query($sql);
+    // $sql = "SELECT * FROM employee";
+    // $result = $connection -> query($sql);
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["number"]. " - name: " . $row["name"].  " - position: ". $row["position"]. " - department: ". $row["department"]. " - supervisor: ". $row["supervisor"]. "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
+    // if ($result->num_rows > 0) {
+    //     // output data of each row
+    //     while($row = $result->fetch_assoc()) {
+    //         echo "id: " . $row["number"]. " - name: " . $row["name"].  " - position: ". $row["position"]. " - department: ". $row["department"]. " - supervisor: ". $row["supervisor"]. "<br>";
+    //     }
+    // } else {
+    //     echo "0 results";
+    // }
     $connection->close();
-    // abcavkjsaskfjksjksdndsfds
-    // abc
-    echo "abc";
-
-    //dsadadasadsad
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+<body>
+    
+</body>
+</html>
