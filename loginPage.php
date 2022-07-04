ffa32221ff011d6c2ad813ac07cba37818b3af9d
ltmh2782 <ltmh2782@gmail.com>
2022-07-04 15:11:30 +0700

    dashboard aside html 040722

--- loginPage.php
+++ loginPage.php
@@ -5,12 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Timeflow Login</title>
-    <link rel="stylesheet" href="style.css">
+    <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="Assets/title-logo.png" />
+    <!-- Style sheet -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class='main'>
