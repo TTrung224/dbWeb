aa4b1587368a68b33f0975856f43fbbda8c6f025
ltmh2782 <ltmh2782@gmail.com>
2022-08-01 07:36:47 +0700

    update todo

--- toDoPage.js
+++ toDoPage.js
@@ -1,5 +1,5 @@
 document.querySelector("#todo-form").addEventListener("mousemove",function(){
-    if(document.querySelector("#todo-form #category").value == "add new"){
+    if(document.querySelector("#todo-form #category").value == "..add new"){
         document.querySelector(".create-cat-form").classList.remove("non-display");
         document.querySelector("#new-task-submit").disabled = true;
     } else {
